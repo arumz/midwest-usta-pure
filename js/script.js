a3c70004a37a7eecaabede0e4074512dcf4c506d
Andrew Roembke <andrewroembke@Andrews-MBP.attlocal.net>
2019-06-12 17:08:26 -0400

    now with animation

--- js/script.js
+++ js/script.js
@@ -5,12 +5,29 @@ document.getElementById("hamburger").addEventListener("click", function() {
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("mobile-documents-link").addEventListener("click", function(){
         document.getElementById("mobile-anchor-container").style.display="none";
+
     });
     document.getElementById("documents-icon").addEventListener("click", function(){
         document.getElementById("mobile-anchor-container").style.display="none";
     });
 
 });
+
+
+var tl = new TimelineLite();
+
+var documentsLink = document.getElementById("mobile-documents-link");
+var documentsIcon = document.getElementById("documents-icon");
+
+
+documentsLink.onclick = function() {
+    tl.to(window, 1.5, {scrollTo:"#footer"});
+};
+documentsIcon.onclick = function() {
+    tl.to(window, 1.5, {scrollTo:"#footer"});
+};
+
+
 
 
 
